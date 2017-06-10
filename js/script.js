f4658253669a80af388abcbef4b832a4725147de
Nabil <duomaster01@gmail.com>
2017-06-09 18:51:58 -0700

    added lightbulb

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("Hello Welcome to Nabil Khalil's Website!");
+var lightSwitch = document.getElementById('lightSwitch');
+var lightBulb = document.getElementById('lightBulb');
+var clicks = 0;
+
+lightSwitch.onclick = function(){
+  clicks++;
+  var clicksQuery = clicks%2;
+  console.log(clicksQuery);
+
+  if (clicksQuery == 0){
+    lightSwitch.src="../images/onSwitch.png";
+    lightBulb.src="../images/lightOn.png"
+  }
+
+  else{
+    lightSwitch.src="../images/offSwitch.png";
+    lightBulb.src="../images/lightOff.png"
+  }
+}
